314515d41147a50e18046fb339b2ab4c16ff51b6
Talfaza <charifyahia4@gmail.com>
2026-01-05 14:38:57 +0100

    add: Tracepoint
    reserve space in ring buffer -> Get Current task to find PPID -> Get Command name -> Send it to golang userspace

--- main.bpf.c
+++ main.bpf.c
@@ -1,6 +1,6 @@
 #include "vmlinux.h"
+#include <bpf/bpf_core_read.h>
 #include <bpf/bpf_helpers.h>
-
 /*
 This data will be sent to golang userspace
 */
@@ -19,4 +19,42 @@ struct {
   __uint(max_entries, 1 << 24); // 16MB buffer
 } events SEC(".maps");
 
-// TODO: Tracepoint
+/*
+Tracepoint attached to execve (the syscall that runs new program)
+*/
+SEC("tracepoint/syscalls/sys_enter_execve")
+int trace_execve(struct trace_event_raw_sys_enter *ctx) {
+  struct event_t *e;
+
+  /*
+  Reserve space in the Ring Buffer
+  If the buffer is full, drop event
+  */
+
+  e = bpf_ringbuf_reserve(&events, sizeof(*e), 0);
+  if (!e) {
+    return 0;
+  }
+
+  /*
+  Get current task struct to find PPID (Parent PID)
+  */
+  struct task_struct *task = (struct task_struct *)bpf_get_current_task();
+
+  e->pid = bpf_get_current_pid_tgid() >> 32;
+  e->uid = bpf_get_current_uid_gid();
+  e->ppid = BPF_CORE_READ(task, real_parent, tgid);
+
+  /*
+  Get the command name
+  */
+  bpf_get_current_comm(&e->command, sizeof(e->command));
+
+  /*
+  Send to golang userspace
+  */
+  bpf_ringbuf_submit(e, 0);
+
+  return 0;
+}
+char LICENSE[] SEC("license") = "Dual MIT/GPL";
