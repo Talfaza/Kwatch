bb6ba2f499bf778c0a55e1fcc051779b7156f0ff
Talfaza <charifyahia4@gmail.com>
2026-01-05 21:56:42 +0100

    add: noise filter, cgroup, arguments
    noise filter on the namespace 0 -> machine 1+ -> container, cgroup for containers, reading arguments

--- main.bpf.c
+++ main.bpf.c
@@ -11,13 +11,12 @@ struct event_t {
   u32 pid;
   u32 ppid;
   u32 uid;
+  u64 cgroup_id;
   u8 command[16];
-} __attribute__((preserve_access_index));
+};
 
-/* Force event_t type to be emitted to BTF
-(needed for bpf2go -type) */
-const struct event_t *__unused_event_t
-    __attribute__((unused, section(".rodata")));
+/* Force event_t type to be emitted to BTF (needed for bpf2go -type) */
+const struct event_t *unused_event_t SEC(".rodata.unused");
 
 /*
 Ring Buffer
@@ -35,32 +34,49 @@ int trace_execve(struct trace_event_raw_sys_enter *ctx) {
   struct event_t *e;
 
   /*
-  Reserve space in the Ring Buffer
-  If the buffer is full, drop event
+  Get current task struct
   */
+  struct task_struct *task = (struct task_struct *)bpf_get_current_task();
 
+  /*
+  Check PID namespace level - containers have level > 0
+  Host processes have level 0, so we skip them
+  */
+  struct nsproxy *ns = BPF_CORE_READ(task, nsproxy);
+  if (!ns) {
+    return 0;
+  }
+
+  struct pid_namespace *pid_ns = BPF_CORE_READ(ns, pid_ns_for_children);
+  if (!pid_ns) {
+    return 0;
+  }
+
+  unsigned int ns_level = BPF_CORE_READ(pid_ns, level);
+  if (ns_level == 0) {
+    return 0; // Skip host processes
+  }
+
+  /*
+  Reserve space in the Ring Buffer
+  */
   e = bpf_ringbuf_reserve(&events, sizeof(*e), 0);
   if (!e) {
     return 0;
   }
 
-  /*
-  Get current task struct to find PPID (Parent PID)
-  */
-  struct task_struct *task = (struct task_struct *)bpf_get_current_task();
-
   e->pid = bpf_get_current_pid_tgid() >> 32;
   e->uid = bpf_get_current_uid_gid();
   e->ppid = BPF_CORE_READ(task, real_parent, tgid);
+  e->cgroup_id = bpf_get_current_cgroup_id();
 
   /*
-  Get the command name
+  Read the filename being executed from execve args
+  args[0] is the filename pointer
   */
-  bpf_get_current_comm(&e->command, sizeof(e->command));
+  const char *filename = (const char *)ctx->args[0];
+  bpf_probe_read_user_str(&e->command, sizeof(e->command), filename);
 
-  /*
-  Send to golang userspace
-  */
   bpf_ringbuf_submit(e, 0);
 
   return 0;
